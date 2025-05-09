53e1065ad8ab854d5278857653549d3feb7a3e79
MCFS436 <596170708@qq.com>
2025-05-09 09:11:15 +0800

    Update smooth_l1.py

--- exercises/smooth_l1.py
+++ exercises/smooth_l1.py
@@ -30,4 +30,8 @@ def smooth_l1(x, sigma=1.0):
     # 3. 对满足条件的元素应用第一个公式 (0.5 * (sigma * x)**2)。
     # 4. 对不满足条件的元素应用第二个公式 (|x| - 0.5 / sigma2)。
     # 5. 可以使用 np.where() 来根据条件选择应用哪个公式。
+    sigma2 = sigma * sigma
+    absx = np.abs(x)
+    lo=np.where(absx < (1/sigma2),0.5* (sigma * x)**2,absx - 0.5 / sigma2)
+    return lo
     pass 
